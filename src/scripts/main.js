35263c59c5a2bce7e98539108b99c83ec82e0f38
amiasso <amiasso@msn.com>
2023-06-15 10:15:53 -0300

    finalizando o projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    //função de sumir com botões
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElentosDoHeader ();
+        }else {
+            exibeElementosDoHeader();
+        }
+    })
     
+        //seção de atrações
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -12,8 +29,34 @@ document.addEventListener('DOMContentLoaded',function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
-})
 
+    //faq
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+})
+//oculta elentos do header
+
+function ocultaElentosDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+//lista de filmes
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
 
